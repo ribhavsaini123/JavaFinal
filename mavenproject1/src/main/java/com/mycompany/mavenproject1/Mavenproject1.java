db385b2e054b39c96f07042c917e1aa5280230b8
ramand111 <ramandeepkaur56@loyalistcollege.com>
2022-06-29 16:47:08 -0400

    Commit From Raman

---
+++ mavenproject1/src/main/java/com/mycompany/mavenproject1/Mavenproject1.java
@@ -0,0 +1,14 @@
+
+
+package com.mycompany.mavenproject1;
+
+/**
+ *
+ * @author erram
+ */
+public class Mavenproject1 {
+
+    public static void main(String[] args) {
+        System.out.println("Raman Here!");
+    }
+}
