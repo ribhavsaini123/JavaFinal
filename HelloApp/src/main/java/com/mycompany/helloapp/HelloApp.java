6f16fb3cfa3175c3416c5715933fe20a1c2477ff
ramand111 <ramandeepkaur56@loyalistcollege.com>
2022-06-29 00:03:49 -0400

    my first commit

---
+++ HelloApp/src/main/java/com/mycompany/helloapp/HelloApp.java
@@ -0,0 +1,13 @@
+
+package com.mycompany.helloapp;
+
+/**
+ *
+ * @author erram
+ */
+public class HelloApp {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
